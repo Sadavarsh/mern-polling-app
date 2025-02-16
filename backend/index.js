07a902c93d9d24ac4cf2a4920cdfa289845c694e
Sadavarsh <sadavarshsaini1@gmail.com>
2025-02-16 13:57:04 +0530

    update CORS_ORIGIN

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: `${process.env.CORS_ORIGIN}` } });
 
 app.use(cors());
 app.use(express.json());
